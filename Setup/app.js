0263107ab5181bd8d0bf9e6330f8c3406096f4e9
neshkovac <lneskovac@hotmail.com>
2020-07-07 15:25:18 +0200

    Fixing magical numbers

--- Setup/app.js
+++ Setup/app.js
@@ -1,21 +1,21 @@
 new Vue({
    el: '#app',
     data: {
-       playerHealth: 100,
-        monsterHealth: 100,
+       PLAYER_HEALTH: 100,
+        MONSTER_HEALTH : 100,
         gameIsRunning: false,
         turns: []
     },
     methods: {
        startGame: function(){
            this.gameIsRunning  = true;
-           this.playerHealth = 100;
-           this.monsterHealth = 100;
+           this.PLAYER_HEALTH = 100;
+           this.MONSTER_HEALTH = 100;
            this.turns = [];
        },
         attack: function(){
            let damage = this.calculateDamage(3,10);
-           this.monsterHealth  -= damage
+           this.MONSTER_HEALTH   -= damage
            this.logDamage(true,damage);
 
            if(this.checkWin()){
@@ -26,7 +26,7 @@ new Vue({
         },
         specialAttack: function(){
            let damage = this.calculateDamage(10,20);
-           this.monsterHealth -= damage;
+           this.MONSTER_HEALTH  -= damage;
            if(this.checkWin()){
                return;
            }
@@ -34,7 +34,7 @@ new Vue({
            this.monsterAttacks();
         },
         heal : function(){
-           if(this.playerHealth <= 90) {
+           if(this.PLAYER_HEALTH  <= 90) {
               this.healByTen();
               this.logHeal();
 
@@ -50,7 +50,7 @@ new Vue({
         },
         monsterAttacks : function(){
            let damage = this.calculateDamage(5,12);
-           this.playerHealth -= damage;
+           this.PLAYER_HEALTH  -= damage;
            this.checkWin();
            this.logDamage(false,damage);
         },
@@ -58,7 +58,7 @@ new Vue({
            return Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
         },
         checkWin : function(){
-            if(this.monsterHealth <= 0) {
+            if(this.MONSTER_HEALTH  <= 0) {
                 if (confirm("You won! New game?")) {
                     this.startGame();
                 }
@@ -68,7 +68,7 @@ new Vue({
                 }
                  return true;
             }
-            else if(this.playerHealth <= 0){
+            else if(this.PLAYER_HEALTH  <= 0){
                 if(confirm('You lost! New game?')){
                     this.startGame()
                 } else {
@@ -92,10 +92,10 @@ new Vue({
            }
         },
         healByTen : function(){
-           this.playerHealth += 10;
+           this.PLAYER_HEALTH  += 10;
         },
         healFullHp: function(){
-           this.playerHealth = 100;
+           this.PLAYER_HEALTH = 100;
         },
         logHeal: function(amount = 10){
            if(amount === 10){
